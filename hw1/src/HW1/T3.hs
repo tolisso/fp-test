1855aa7521a96a669885d8729de2dcfe4cae47c7
tolisso <tolisso@e.malko@bk.ru>
2021-10-25 16:51:59 +0300

    few changes commit

--- hw1/src/HW1/T3.hs
+++ hw1/src/HW1/T3.hs
@@ -1,10 +1,7 @@
 module HW1.T3 where
 
 data Tree a = Leaf | Branch (Int, Int) (Tree a) a (Tree a)
-
--- instance Show a => Show (Tree a) where 
-    -- show Leaf = ""
-    -- show (Branch _ l x r) = "(" ++ show l ++ " /" ++ show x ++ "/" ++ show (max (tdepth l) (tdepth r) + 1) ++ "\\ " ++ show r ++ ")"
+-- Branch (size, depth) left value right
 
 tsize :: Tree a -> Int
 tsize Leaf = 0
@@ -53,18 +50,6 @@ tbalance (Branch _ l x r)
     | tdepth r - tdepth l > 1 
         = trotateRL (mkBranch l x r)
 tbalance t = t
-
--- dl = ll + 1
--- dl - dr > 1
--- ll - dr > 0
--- tbalance :: Tree a -> Tree a
--- tbalance t = foldl (\tr (s1, s2, method) -> 
---     if tbalanced tr s1 s2 
---         then tr 
---         else method tr) t 
---     [(LeftSide, LeftSide, trotateR), (RightSide, RightSide, trotateL),
---     (RightSide, LeftSide, trotateLR), (LeftSide, RightSide, trotateRL)]
-    
 
 tinpush :: Ord a => a -> Tree a -> Tree a
 tinpush x Leaf = mkBranch Leaf x Leaf
