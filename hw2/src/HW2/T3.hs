f4933bbae27bebebd15ff31e64c6b9e26b801e4d
tolisso <tolisso@e.malko@bk.ru>
2021-11-03 18:46:34 +0300

    HW2: T3 added

---
+++ hw2/src/HW2/T3.hs
@@ -0,0 +1,26 @@
+module HW2.T3 where
+
+import HW2.T1
+
+joinOption :: Option (Option a) -> Option a
+joinOption None = None
+joinOption (Some None) = None
+joinOption (Some (Some x)) = Some x
+
+joinExcept :: Except e (Except e a) -> Except e a
+joinExcept (Error e) = Error e
+joinExcept (Success (Error e)) = Error e
+joinExcept (Success (Success x)) = Success x
+
+joinAnnotated :: Semigroup e => Annotated e (Annotated e a) -> Annotated e a
+joinAnnotated ((x :# e1) :# e2) = x :# e1 <> e2;
+
+joinList :: List (List a) -> List a
+joinList Nil = Nil
+joinList (Nil :. rest) = joinList rest
+joinList ((x :. inrest) :. rest) = x :. joinList (inrest :. rest)
+
+joinFun :: Fun i (Fun i a) -> Fun i a
+joinFun (F f) = F $ getF <*> f where 
+    getF :: i -> (Fun i a) -> a
+    getF x (F g) = g x
